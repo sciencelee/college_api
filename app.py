f9a3c8542fb2708a170cce3f16278632632df6a9
Aaron Lee <amlee73@gmail.com>
2021-06-11 15:30:40 -0500

    added initial files

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ import random
 app = Flask(__name__, static_url_path="/static")
 
 @app.route('/')
-def matches(colleges):
+def index(colleges):
     return {'University of Michigan':100, 'University of Illinois':88, 'DePaul University':82}
 
 
