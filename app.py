4204e49e15ca0fff8e36225730f5790856a7d27b
Aaron Lee <amlee73@gmail.com>
2021-06-11 20:35:29 -0500

    added initial files

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# routes
-@app.route('/model/', methods=['POST', 'GET'])
-def predict():
-    # get data
-    #data = request.get_json(force=True)
-
-    # convert data into dataframe
-    # data.update((x, [y]) for x, y in data.items())
-    # data_df = pd.DataFrame.from_dict(data)
-
-    # predictions
-    # result = model.predict(data_df)
-
-    # send back to browser
-    # output = {'results': int(result[0])}
-
-
-    # return data
-    return jsonify({'University of Michigan':90, 'University of Illinois':80, 'DePaul University': 70})
+# # routes
+# @app.route('/model/', methods=['POST', 'GET'])
+# def predict():
+#     # get data
+#     #data = request.get_json(force=True)
+#
+#     # convert data into dataframe
+#     # data.update((x, [y]) for x, y in data.items())
+#     # data_df = pd.DataFrame.from_dict(data)
+#
+#     # predictions
+#     # result = model.predict(data_df)
+#
+#     # send back to browser
+#     # output = {'results': int(result[0])}
+#
+#
+#     # return data
+#     return jsonify({'University of Michigan':90, 'University of Illinois':80, 'DePaul University': 70})
 
 
 # A welcome message to test our server
@@ -33,4 +33,4 @@ def index():
     return "<h1>College rec server</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True)
