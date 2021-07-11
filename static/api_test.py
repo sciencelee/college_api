d052185892173c1ef11d47efad2a7ca796dda88b
Aaron Lee <amlee73@gmail.com>
2021-07-11 11:15:05 -0500

    attemption web scraping of all colleges to get description and image to feature on front end cards

--- static/api_test.py
+++ static/api_test.py
@@ -14,14 +14,14 @@ school_data = [
     },
 ]
 
-url = 'https://college-rec-system.herokuapp.com/model/'
-#url = 'http://127.0.0.1:5000/model/'
+#url = 'https://college-rec-system.herokuapp.com/model/'
+url = 'http://127.0.0.1:5000/model/'
 
 data = requests.post(url, json=school_data)
 print(data.text)
 
-url2 = 'https://college-rec-system.herokuapp.com/colleges/'
-#url2 = 'http://127.0.0.1:5000/colleges/'
+#url2 = 'https://college-rec-system.herokuapp.com/colleges/'
+url2 = 'http://127.0.0.1:5000/colleges/'
 colleges = requests.get(url2)
 print(colleges.text)
 #
